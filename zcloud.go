9602c3537a680c53c87d4c4f001175417f02ecb2
Nate Wilkinson <nathanwilk7@gmail.com>
2018-03-28 22:32:37 -0600

    normal main

--- zcloud.go
+++ zcloud.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"github.com/nathanwilk7/zcloud/cmd"
-	"github.com/nathanwilk7/zcloud/controller"
-	"github.com/nathanwilk7/zcloud/out"
 )
 
 func main () {
-	//cmd.RootCmd.Execute()
-	pp := cmd.GetProvParamsFromEnv()
-	sp := controller.SyncParams{
-		Src: "cloud://zcloud-testing/",
-		Dest: "testdata",
-	}
-	controller.Sync(pp, sp, out.New())
+	cmd.RootCmd.Execute()
 }
